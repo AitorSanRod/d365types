4ec0d9b10fb4c0e42c85c5d819f30e328302b4d3
Aitor <aitor.san.rodriguez@gmail.com>
2024-12-21 23:51:54 +0100

    NPM docs

---
+++ helloWorld.js
@@ -0,0 +1,9 @@
+/// <reference path="types/d365.d.ts" />
+/**
+ * @param {D365.ExecutionContext} executionContext
+ */
+
+function Example(executionContext) {
+    const formContext = executionContext.getFormContext();
+    const name =  formContext.getAttribute("name").getValue();
+}
