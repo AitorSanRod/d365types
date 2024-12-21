81770e2e8c82ffbeb363514b2dee8414d6238a13
Aitor <aitor.san.rodriguez@gmail.com>
2024-12-21 22:23:20 +0100

    gitignore

--- index.js
+++
@@ -1,9 +0,0 @@
-/// <reference path="types/d365.d.ts" />
-/**
- * @param {D365.ExecutionContext} executionContext
- */
-
-function OnLoad(executionContext) {
-    const formContext = executionContext.getFormContext();
-    const name =  formContext.getAttribute("name").getValue();
-}
